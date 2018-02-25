5e8f11e8da34bf90f2b5f408b1b3c89ad826dead
James Dindi <dindijjames@gmail.com>
2018-02-25 07:45:08 +0300

    Use componentWillMount - A life cycle method Fires off every time the component is rerendered

--- src/App.js
+++ src/App.js
@@ -6,26 +6,30 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      projects: [
-        {
-          title: 'Business Website',
-          category: 'Web Design'
-        },
-        {
-          title: 'Social App',
-          category: 'Mobile Development'
-        },
-        {
-          title: 'Ecommerce Shoppping Cart',
-          category: 'Web Development'
-        }
-      ]
+      projects: []
     }
+  }
+
+  componentWillMount(){
+    this.setState({projects: [
+      {
+        title: 'Business Website',
+        category: 'Web Design'
+      },
+      {
+        title: 'Social App',
+        category: 'Mobile Development'
+      },
+      {
+        title: 'Ecommerce Shoppping Cart',
+        category: 'Web Development'
+      }
+    ]});
   }
   render() {
     return (
       <div className="App">
-        My App
+        <h2>My App</h2>
         <Projects projects={this.state.projects} />
       </div>
     );
